1b710bb55025b302f2444b6b4a15bab898955fb8
nozomi.takahashi <mir_0112@yahoo.co.jp>
2021-05-19 23:06:19 +0900

    Add validation

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -2,8 +2,10 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
 import utils.DButil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class CreateServlet
@@ -46,13 +49,28 @@ public class CreateServlet extends HttpServlet {
             t.setCreated_at(currentTime);
             t.setUpdated_at(currentTime);
 
-            em.getTransaction().begin();
-            em.persist(t);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
+            //バリデーションを実行、エラーがあったら戻る
+            List<String> errors = MessageValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
 
-            response.sendRedirect(request.getContextPath() + "/index");
+                //フォームに初期値設定、エラーメッセージ
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/new.jsp");
+                rd.forward(request, response);
+            } else {
+                //DBに保存
+                em.getTransaction().begin();
+                em.persist(t);
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "登録が完了しました。");
+                em.close();
+
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 }
--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -1,9 +1,12 @@
 package controllers;
 
+
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,6 +15,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import models.Task;
 import utils.DButil;
+import validators.MessageValidator;
 
 /**
  * Servlet implementation class UpdateServlet
@@ -46,17 +50,29 @@ public class UpdateServlet extends HttpServlet {
             Timestamp currentTime = new Timestamp(System.currentTimeMillis());
             t.setUpdated_at(currentTime);
 
-            //DB更新
-            em.getTransaction().begin();
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "更新が完了しました。");
-            em.close();
+            List<String> errors = MessageValidator.validate(t);
+            if(errors.size() > 0) {
+                em.close();
 
-            //セッションスコープ上の不要になったデータを削除
-            request.getSession().removeAttribute("task_id");
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("task", t);
+                request.setAttribute("errors", errors);
 
-            //indexへリダイレクト
-            response.sendRedirect(request.getContextPath() + "/index");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                //DB更新
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+                em.close();
+
+                //セッションスコープ上の不要になったデータを削除
+                request.getSession().removeAttribute("task_id");
+
+                //indexへリダイレクト
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
         }
     }
 }
